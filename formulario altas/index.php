81ad8eb2fefb06457617fbd49f4efc56b733cf0b
David Gonzalez <div.divad990@gmail.com>
2024-02-20 21:01:42 +0100

    Agregado resalte menu

--- formulario altas/index.php
+++ formulario altas/index.php
@@ -1,3 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+            <section id="hs1">
+                <div class="hsd1"><img src="../img/1.jpg">RUINASA S.A.</div>
+            </section>
+            <section id="hs2">
+                <div class="hsd2"><nav id="menu">
+                    <ul>
+                    <li><a href="">Inicio</a></li>
+                    <li id="activo"><a href="">Formulario</a>
+                    <li><a href="">Empleados</a></li>
+                    <li><a href="">Clientes</a></li>
+                </ul></nav></div>
+            </section>
+        </header>
+    </body>
+</html>
+
 <?php
 echo "Prueba";
 ?>
